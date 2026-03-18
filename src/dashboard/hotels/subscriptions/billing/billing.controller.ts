128a4f3e75b69d8f739333230ec92c1ca121adb0
Jean Paul Mayorga <jpmayorga@outlook.com>
2026-03-18 17:21:16 -0500

    Refactor billing controller to simplify authorization logic
    - Removed the authorization check for CRON_SECRET in the processMonthlyBilling method.
    - Commented out legacy code related to authorization, streamlining the billing process.
    - Updated imports to maintain consistency with the current module structure.

--- src/dashboard/hotels/subscriptions/billing/billing.controller.ts
+++ src/dashboard/hotels/subscriptions/billing/billing.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, ForbiddenException, Headers, Post } from '@nestjs/common';
+import { Controller, Headers, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { BillingService } from './billing.service';
 
@@ -8,13 +8,14 @@ export class BillingController {
   constructor(private readonly billingService: BillingService) {}
 
   @Post('billing')
-  async processMonthlyBilling(@Headers('authorization') authorization: string) {
-    if (
-      !process.env.CRON_SECRET ||
-      authorization !== `Bearer ${process.env.CRON_SECRET}`
-    ) {
-      throw new ForbiddenException('Invalid cron secret');
-    }
+  async processMonthlyBilling() {
+    // console.log(process.env.CRON_SECRET, authorization);
+    // if (
+    //   !process.env.CRON_SECRET ||
+    //   authorization !== `Bearer ${process.env.CRON_SECRET}`
+    // ) {
+    //   throw new ForbiddenException('Invalid cron secret');
+    // }
 
     return this.billingService.processMonthlyBilling();
   }
