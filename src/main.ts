aab4c6bb82e5b0ac97e8152b2054b951b7d33750
Jean Paul Mayorga <jpmayorga@outlook.com>
2026-03-18 01:50:42 -0500

    Remove production-specific logging configuration from main application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-if (process.env.NODE_ENV === 'production') {
-  process.env.NO_COLOR = '1';
-}
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
